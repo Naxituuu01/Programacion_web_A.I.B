86dfb524d463aca667c9f9c435187d622d5ae6a0
li3b33 <134418801+AlanBarria@users.noreply.github.com>
2024-05-17 00:03:49 -0400

    v9
    -Anterior mente se había eliminado por comodidad ya que la subí mal y se rompió todo

---
+++ presentacion/js/js.js
@@ -0,0 +1,57 @@
+function inicia(){
+
+    let user=document.getElementById('user').value;
+    let pass=document.getElementById('pass').value;
+
+    if(user== 'Alan' & pass== '1234'){
+
+    window.location="../html/index.html";
+
+    }
+    else{
+
+        alert("Datos Incorrescotos")
+        document.getElementById('user').value=''; 
+        document.getElementById('pass').value='';
+    }
+
+}
+
+//-----------------------------------------------------------
+
+const modalContainer = document.getElementById('contenedor-modal');
+const closeButton = document.getElementById('cerrar-modal');
+
+
+function abrirModal() {
+  modalContainer.style.display = 'block';
+}
+
+
+function cerrarModal() {
+  modalContainer.style.display = 'none';
+}
+
+
+const triggerElement = document.getElementById('boton-modal');
+triggerElement.addEventListener('click', abrirModal);
+closeButton.addEventListener('click', cerrarModal);
+
+//---------------------------------------------------
+function datos(){
+
+  let mail=document.getElementById('mail').value;
+  let fono=document.getElementById('fono').value;
+  let c=document.getElementById('c').value;
+  let nombre=document.getElementById('nombre').value;
+
+  if(nombre!='' & mail!='' & fono !='' & com!=''){
+      alert("Datos enviados");
+  }
+  
+  else{
+      alert("Rellene todas las casillas")
+  }
+};
+
+
